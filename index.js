ea6646e318419fa93225d574b1ae6947f30fa2a6
marcus110379 <marcusjmadden@gmail.com>
2019-08-19 14:08:58 -0500

    ic

---
+++ index.js
@@ -0,0 +1,56 @@
+// alert("test");
+// Try to do this without using the code from previous work. Only use the JavaScript reference guide: https://docs.google.com/document/d/1sdPHe4xQt_qEzbePtZRdTOcX2hDWpBVV8E_L8JvcPrI/
+//
+//     Create a main function for all of your code. Keep asking the user to press 1, 2, or 3 until they press 'q' to quit.
+//
+// - If they press 1, alert "Challenge goes here".
+// - If they press 2, alert "2".
+// - If they press 3, alert "Hello World".
+// function main() {
+//     let userInput = "";
+//     while (userInput !== "q") {
+//         userInput = prompt("press 1, 2, or 3 until they press 'q' to quit");
+//         if (parseInt(userInput) === 1) {
+//             alert("Challenge goes here");
+//         } else if (parseInt(userInput) === 2) {
+//             console.log(2);
+//         } else if (parseInt(userInput) === 3) {
+//             alert("hello world")
+//
+//         }
+//     }
+// }
+// main();
+
+
+//if they press 1, ask them to press a, b, or c in a different function. If they press a, alert "A", if they press b, alert "B", if they press c, alert "C".
+// function main() {
+//     let userInput = "";
+//     while (userInput !== "q") {
+//         userInput = prompt("press 1, 2, or 3 until they press 'q' to quit");
+//         if (parseInt(userInput) === 1) {
+//             userInput2();
+//
+//         } else if (parseInt(userInput) === 2) {
+//             console.log(2);
+//         } else if (parseInt(userInput) === 3) {
+//             alert("hello world")
+//
+//         }
+//     }
+// }
+// main();
+// function userInput2() {
+//     let userDecision = prompt(" press a, b or c");
+//     if(userDecision === "a"){
+//         alert("A");
+//     }else if(userDecision === "b"){
+//         alert("B");
+//     }else if (userDecision){
+//         alert("C");
+//     }
+// }
+
+// ### Challenge 2
+// - Return the values of a, b, or c to use in the main function.
+
